ad6d9c2523c293fb82f52c3e09b54ef48cefaccb
EAndriano <a01747979@tec.mx>
2023-04-24 09:09:27 -0600

    Set width and height to `10px` of sketch divs

--- code.js
+++ code.js
@@ -5,6 +5,8 @@ MAX = 10;
 for(i = 0; i < MAX; i++){
     div = document.createElement('div');
     div.classList.add('sketch');
+    div.style.width = '10px';
+    div.style.height = '10px';
     div.textContent = 'O';
 
     sketchpad.appendChild(div);
