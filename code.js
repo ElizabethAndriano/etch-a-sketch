c6c2e8dc379817077c31dab434eadd78af5fa444
EAndriano <a01747979@tec.mx>
2023-04-25 13:25:29 -0600

    Add style to html

--- code.js
+++ code.js
@@ -1,13 +1,14 @@
 sketchpad = document.querySelector('#sketchpad');
 
-MAX = 10;
+MAX = 16*16;
 
 for(i = 0; i < MAX; i++){
     div = document.createElement('div');
-    div.classList.add('sketch');
+
+    i % 2 == 0 ? div.classList.add('sketch') : div.classList.add('sketch1');
+
     div.style.width = '10px';
     div.style.height = '10px';
-    div.textContent = 'O';
 
     sketchpad.appendChild(div);
     console.log(i)
